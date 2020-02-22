bf1ece22448cda7b0de999f0666b22d06c1e740e
Mariana Gogashvili <marianagogashvili24@gmail.com>
2020-02-22 22:11:53 +0200

    second tab of film search

--- src/app/films/search/search.component.ts
+++ src/app/films/search/search.component.ts
@@ -2,6 +2,11 @@ import { Component, Input, OnInit, Output } from '@angular/core';
 import { Subject } from 'rxjs';
 import { FormControl } from '@angular/forms'; 
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as faStar2} from '@fortawesome/free-solid-svg-icons';
+
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeart2} from '@fortawesome/free-solid-svg-icons';
 
 import { FilmService } from '../film.service';
 @Component({
@@ -28,6 +33,16 @@ export class SearchComponent implements OnInit {
   filmTitle: FormControl = new FormControl();
   films;
   isHidden=true;
+  /////
+  secondTabHidden=true;
+  currentFilm;
+  //////
+  starIcon = faStar; starIcon2 = faStar; starIcon3 = faStar; starIcon4 = faStar; starIcon5 = faStar;
+  heartIcon = faHeart;
+  liked = false;
+  rating = 0;
+
+  date = new Date();
   constructor(private filmService: FilmService) { }
 
   ngOnInit() {
@@ -51,5 +66,62 @@ export class SearchComponent implements OnInit {
 
   addFilm(film) {
   	console.log(film);
+  	this.currentFilm = film;
+  	this.secondTabHidden = false;
   }
+
+  changeStarIcon(num) {
+  	this.rating = num;
+	  	if (num === 1) {
+	  		this.starIcon = faStar2;
+	  		this.starIcon2 = faStar;
+	  		this.starIcon3 = faStar;
+	  		this.starIcon4 = faStar;
+	  		this.starIcon5 = faStar;
+	  	} else if(num === 2) {
+	  		this.starIcon = faStar2;
+	  		this.starIcon2 = faStar2;
+	  		this.starIcon3 = faStar;
+	  		this.starIcon4 = faStar;
+	  		this.starIcon5 = faStar;
+	  	} else if(num === 3) {
+	  		this.starIcon = faStar2;
+	  		this.starIcon2 = faStar2;
+	  		this.starIcon3 = faStar2;
+	  		this.starIcon4 = faStar;
+	  		this.starIcon5 = faStar;
+	  	} else if(num === 4) {
+	  		this.starIcon = faStar2;
+	  		this.starIcon2 = faStar2;
+	  		this.starIcon3 = faStar2;
+	  		this.starIcon4 = faStar2;
+	  		this.starIcon5 = faStar;
+	  	} else if(num === 5) {
+	  		this.starIcon = faStar2;
+	  		this.starIcon2 = faStar2;
+	  		this.starIcon3 = faStar2;
+	  		this.starIcon4 = faStar2;
+	  		this.starIcon5 = faStar2;
+	  	} 
+  }
+
+  changeHeartIcon() {
+  	this.liked = !this.liked;
+  	if (this.heartIcon === faHeart) {
+  		this.heartIcon = faHeart2;
+  	} else if (this.heartIcon === faHeart2) {
+  		this.heartIcon = faHeart;
+  	}
+  }
+  saveReview(id, date, review) {
+  	console.log(id);
+  	console.log(date);
+  	console.log(review);
+
+  }
+
+  goBack() {
+  	this.secondTabHidden = true;
+  }
+
 }
