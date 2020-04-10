d7ebf6e3b317923b21b476175fe994b0a530ebd2
Mariana Gogashvili <marianagogashvili24@gmail.com>
2020-04-10 20:58:15 +0300

    new list continued

--- src/app/lists/new-list/new-list.component.ts
+++ src/app/lists/new-list/new-list.component.ts
@@ -4,6 +4,8 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FilmService } from '../../films/film.service';
 import { FormControl } from '@angular/forms'; 
 import { BehaviorSubject } from 'rxjs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <== add the imports!
+
 @Component({
   selector: 'app-new-list',
   templateUrl: './new-list.component.html',
@@ -15,7 +17,8 @@ export class NewListComponent implements OnInit {
   filmName: FormControl = new FormControl();
   films;
   listFilms = [];
-  filmSubject = new BehaviorSubject(null);
+  rankedList = false;
+  // filmSubject = new BehaviorSubject(null);
 
   constructor(private filmService: FilmService) { }
 
@@ -24,45 +27,53 @@ export class NewListComponent implements OnInit {
 	  		if (title !== '' && (title.split('').length >= 3)) {
 		  		this.filmService.findFilms(title).subscribe(result => {
 		  			this.films = result;
-		  			console.log(result);
+		  			// console.log(result);
 		  		});
 		  	} else {
 				this.films = [];
 		  	}
 		  	
 	  	});
-    this.filmSubject.subscribe(result => {
-    	console.log(result);
-    	if (result !== null) {
-    		this.listFilms.push(result);
-    	}
-    });
+    // this.filmSubject.subscribe(result => {
+    // 	console.log(result);
+    // 	if (result !== null) {
+    // 		// this.listFilms.push(result);
+    // 		this.listFilms = result;
+    // 	}
+    // });
   }
 
   addToList(film) {
-  	// this.listFilms.forEach(result => {
-  	// 	console.log(result);
-  	// 	if (film['id'] !== result['id']) {
-  	// 		this.filmSubject.next(film);
-  	// 	}
-  	// });
-  	// this.filmSubject.next(film);
-  	// console.log(this.listFilms.indexOf(film) != -1);
-	let add;
-
+	  let add=0;
   	if (this.listFilms.length !== 0) {
   		Object.values(this.listFilms).forEach(result => {
-  			console.log(result['id'] === film['id']);
-  			add = (result['id'] === film['id']);
+  			if (result['id'] === film['id']){
+  				add=add+1;
+  			} 
 	  	});
-	  	if (add === false) {
-	  		this.filmSubject.next(film);
+	  	if (add === 0) {
+	  		this.listFilms.push(film);
+	  		// this.filmSubject.next(this.listFilms);
 	  	}
   	} else {
-  		console.log('FILM2');
-  		this.filmSubject.next(film);
+	  	this.listFilms.push(film);
+  		// this.filmSubject.next(this.listFilms);
   	}
   	
   }
 
+  removeFilm(film) {
+		Object.values(this.listFilms).forEach((result, i) => {
+  			if (result['id'] === film['id']){
+  				this.listFilms.splice(i, 1);
+  				// this.filmSubject.next(film);
+  			} 
+	  	});
+  }
+
+  switchRanked() {
+    this.rankedList = !this.rankedList;
+  }
 }
+
+
