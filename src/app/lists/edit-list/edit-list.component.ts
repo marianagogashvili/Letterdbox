3e859f1e8cb93e0ece3439ced1e10f6476a205db
Mariana Gogashvili <marianagogashvili24@gmail.com>
2020-04-13 22:25:19 +0300

    edit list continued

--- src/app/lists/edit-list/edit-list.component.ts
+++ src/app/lists/edit-list/edit-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FilmService } from '../../films/film.service';
@@ -13,7 +13,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './edit-list.component.html',
   styleUrls: ['./edit-list.component.css']
 })
-export class EditListComponent implements OnInit {
+export class EditListComponent implements OnInit, OnDestroy {
   starIcon = faStar;
   crossIcon = faTimes;
   filmName: FormControl = new FormControl();
@@ -27,29 +27,39 @@ export class EditListComponent implements OnInit {
   listPublic;
   rankedList;
 
-  initFilms;
+  initFilms = [];
   initList;
+
+  sub1; sub2;
 
   constructor(private filmService: FilmService,
               private listService: ListService,
               private route: ActivatedRoute) { }
 
     ngOnInit() {
-    	this.route.params.subscribe(params => {
+    	
+    	this.sub1 = this.route.params.subscribe(params => {
     		let id = +params['id'];
 	    	this.listService.findList({id: id}).subscribe(result => {
 	    		// console.log(result);
+	    		Object.values(result[1]).forEach(film => {
+	    			this.initFilms.push(+film['id']);
+	    		});
+    			
 	    		this.initList = result[0];
-	    		this.initFilms = result[1];
-
+	    		
 	    		this.listName = result[0]['title'];
 	    		this.listDescr = result[0]['description'];
 				this.listPublic = result[0]['public'];
 				this.rankedList = result[0]['ranked'];
 				this.listFilms = result[1];
+				// console.log("RESULT !", result[1]);
+				// console.log("RESULT !2", this.initFilms);
+				// console.log("RESULT !3", this.listFilms);
+
 	    	});
 		});
-		this.filmName.valueChanges.subscribe(title => {
+		this.sub2 = this.filmName.valueChanges.subscribe(title => {
 	  		if (title !== '' && (title.split('').length >= 3)) {
 		  		this.filmService.findFilms(title).subscribe(result => {
 		  			this.films = result;
@@ -59,7 +69,7 @@ export class EditListComponent implements OnInit {
 				this.films = [];
 		  	}
 	  	});
-		console.log(this.initFilms);
+		// console.log(this.initFilms);
 	}
     // this.filmSubject.subscribe(result => {
     // 	console.log(result);
@@ -89,22 +99,30 @@ export class EditListComponent implements OnInit {
   	} else {
 	  	this.listFilms.push(film);
   	}
-  	
+	// console.log("RESULT !2", this.initFilms);
+	// console.log("RESULT !3", this.listFilms);
+
   }
 
   removeFilm(film) {
-		Object.values(this.listFilms).forEach((result, i) => {
-  			if (result['id'] === film['id']){
-  				this.listFilms.splice(i, 1);
-  				// this.filmSubject.next(film);
-  			} 
-	  	});
+	Object.values(this.listFilms).forEach((result, i) => {
+			if (result['id'] === film['id']){
+				this.listFilms.splice(i, 1);
+				// this.filmSubject.next(film);
+			} 
+  	});
+	// console.log("RESULT !2", this.initFilms);
+	// console.log("RESULT !3", this.listFilms);
+
   }
 
   switchRanked() {
     this.rankedList = !this.rankedList;
   }
   saveList(form: NgForm) {
+  	console.log("bRUH 1",this.initFilms);
+	console.log("bRUH 2",this.listFilms);
+    
     if (this.listFilms.length === 0) {
       this.error = "Please select films to create a list";
       setTimeout(() => {
@@ -121,19 +139,35 @@ export class EditListComponent implements OnInit {
                    ranked: form.value.ranked,
                    user_id: this.currentUserId,
                    films: films}
-      console.log(param);
-   //    console.log(this.initList['title'] === form.value.list);
-   //    console.log(this.initList['description'] === form.value.descr);
-	  // console.log(this.initList['public'] === form.value.public);
-	  // console.log(this.initList['ranked'] === form.value.ranked);
-	  console.log(this.initFilms);
-	  console.log(this.listFilms);
 
-	  console.log(this.initFilms === this.listFilms);
+      console.log(this.initList);
+      console.log(form.value.list);
+      console.log(this.initList['description'] === form.value.descr);
+	  console.log(this.initList['public'] === form.value.public);
+	  console.log(this.initList['ranked'] === form.value.ranked);
+	  console.log(this.initFilms === films);
+	  console.log(this.initFilms);
+	  console.log(films);
+	  console.log(JSON.stringify(this.initFilms) === JSON.stringify(films));
+	  console.log();
+
+	  if ((this.initList['title'] === form.value.list) && 
+	  	  (JSON.stringify(this.initFilms) === JSON.stringify(films)) &&
+	  	  (this.initList['description'] === form.value.descr) &&
+	  	  (this.initList['public'] === form.value.public) &&
+	  	  (this.initList['ranked'] === form.value.ranked)) {
+	  	this.error = "Sorry, you didn't change anything";
+	  } else if((JSON.stringify(this.initFilms) === JSON.stringify(films))) {
+
+	  }
       // this.listService.updateList(param).subscribe(result => {
       //   console.log(result);
       // });
     }
   }
 
+  ngOnDestroy() {
+  	this.sub1.unsubscribe();
+  	this.sub2.unsubscribe();
+  }
 }
